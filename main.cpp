ecade4dbeddeed1f8f5c68de637a0c8757d8153d
LIUTAO <73093214+byliutao@users.noreply.github.com>
2023-05-04 20:08:16 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,12 @@ using namespace std;
 
 int main(){
     string model_path = "../model/v20_gamma_64_641.xml";
-    OpenvinoInfer openvinoInfer(model_path,"CPU");
+    OpenvinoInfer openvinoInfer(model_path,"GPU");
     Mat img = imread("../imgs/3.jpg");
     cvtColor(img,img,cv::COLOR_BGR2GRAY);
+    double t1 = cv::getTickCount();
     openvinoInfer.infer(img);
+    double t2 = cv::getTickCount();
+    std::cout <<"time: "<< (t2 - t1) / cv::getTickFrequency() * 1000 << " "<<std::endl;
     return 0;
 }
