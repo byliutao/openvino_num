ec23b1e10dade2a76a52bb466f71555e73e07faa
LIUTAO <73093214+byliutao@users.noreply.github.com>
2023-05-07 20:09:19 +0800

    Update OpenvinoInfer.h

--- OpenvinoInfer.h
+++ OpenvinoInfer.h
@@ -9,15 +9,20 @@
 #include <openvino/openvino.hpp>
 #include <vector>
 
+//#define num
+
 //用前先根据模型修改好要修改的地方
 
 using namespace cv;
 using namespace std;
 class OpenvinoInfer {
-
-    const int IMAGE_HEIGHT = 64;
-    const int IMAGE_WIDTH = 64;
-
+#ifdef num
+    const int IMAGE_HEIGHT = 32;
+    const int IMAGE_WIDTH = 32;
+#else
+    const int IMAGE_HEIGHT = 640;
+    const int IMAGE_WIDTH = 640;
+#endif
     std::shared_ptr<ov::Model> model;
     ov::Core core;
     ov::preprocess::PrePostProcessor *ppp;
@@ -25,16 +30,26 @@ class OpenvinoInfer {
     ov::Shape input_shape;
 public:
     OpenvinoInfer(string model_path, string device){
+#ifdef num
         input_shape = {1, static_cast<unsigned long>(IMAGE_HEIGHT), static_cast<unsigned long>(IMAGE_WIDTH), 1};
+#else
+        input_shape = {1, static_cast<unsigned long>(IMAGE_HEIGHT), static_cast<unsigned long>(IMAGE_WIDTH), 1};
+#endif
         model = core.read_model(model_path);
         // Step . Inizialize Preprocessing for the model
         ppp = new ov::preprocess::PrePostProcessor(model);
-        // Specify input image format
+//        // Specify input image format
+#ifdef num
         ppp->input().tensor().set_shape(input_shape).set_element_type(ov::element::u8).set_layout("NHWC");
         ppp->input().preprocess().resize(ov::preprocess::ResizeAlgorithm::RESIZE_LINEAR);
-
-        //  Specify model's input layout
         ppp->input().model().set_layout("NHWC");
+#else
+        ppp->input().tensor().set_element_type(ov::element::u8).set_layout("NHWC").set_color_format(ov::preprocess::ColorFormat::BGR);
+        // Specify preprocess pipeline to input image without resizing
+        ppp->input().preprocess().convert_element_type(ov::element::f32).convert_color(ov::preprocess::ColorFormat::RGB).scale({255., 255., 255.});
+        //  Specify model's input layout
+        ppp->input().model().set_layout("NCHW");
+#endif
         // Specify output results format
         ppp->output().tensor().set_element_type(ov::element::f32);
         // Embed above steps in the graph
@@ -61,8 +76,10 @@ public:
         // Step 6. Create an infer request for model inference
         ov::InferRequest infer_request = compiled_model.create_infer_request();
         infer_request.set_input_tensor(input_tensor);
+        double ta = cv::getTickCount();
         infer_request.infer();
-
+        double tb = cv::getTickCount();
+        std::cout <<"timeab: "<< (tb - ta) / cv::getTickFrequency() * 1000 << " "<<std::endl;
 
         //Step 7. Retrieve inference results
         const ov::Tensor &output_tensor = infer_request.get_output_tensor();
